5ef3eb2d57a0a7e5ff286352b7db0aa7035c9c6b
Ray Skinner <mr.rayskinner@gmail.com>
2015-11-05 15:18:35 -0600

    Randomize Button
    Added Randomize Button so you can demo in the editor faster.

---
+++ Assets/Randomize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Randomize : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public void Randomness()
+    {
+        Application.LoadLevel(0);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
